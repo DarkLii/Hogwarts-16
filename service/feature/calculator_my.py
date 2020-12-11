f6cd272124dd1c3e5ade716351886b2b9fc10c2e
DarkLii <Lgm@38663508>
2020-12-11 22:07:59 +0800

    1、加减乘除单元测试

---
+++ service/feature/calculator_my.py
@@ -0,0 +1,20 @@
+# -*- coding:utf-8 -*-
+
+
+# 实现计算器功能，用于 Hogwarts-16.service.calculator:Calculator 单元测试
+class CalculatorMy:
+    def add(self, a, b):
+        # ToDO 实现 加法 运算
+        pass
+
+    def sub(self, a, b):
+        # ToDO 实现 减法 运算
+        pass
+
+    def mul(self, a, b):
+        # ToDO 实现 乘法 运算
+        pass
+
+    def div(self, a, b):
+        # ToDO 实现 除法 运算
+        pass
